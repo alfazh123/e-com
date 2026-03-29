941d4bbc59393b8bcc29a9d9904add5c0fa7ecd2
alfazh123 <alfazh291@gmail.com>
2026-03-29 15:44:38 +0700

    fix group product and promo list

--- app/components/productGroup.tsx
+++ app/components/productGroup.tsx
@@ -2,11 +2,16 @@ import ProductList from "./productList";
 
 export default function ProductGroup() {
     return (
-        <div className="container mx-auto px-4 py-8">
-            <h1 className="text-3xl font-bold mb-6">Our Products</h1>
-            <ProductList itemType="pulsa" />
-            <div className="my-8" />
-            <ProductList itemType="data" />
-        </div>
-    )
+			<div className="w-full max-w-6xl mx-auto overflow-clip md:p-8 p-4 bg-blue-950 md:rounded-2xl my-20">
+				<div className="relative ">
+					<div className="bg-blue-500/30 h-96 w-80 absolute -top-60 -left-10 z-0 rounded-full" />
+					<div className="bg-blue-200/30 h-62 w-62 absolute top-10 left-10 z-0 rounded-full" />
+					<ProductList itemType="pulsa" />
+					<div className="my-8" />
+					<ProductList itemType="data" />
+					<div className="bg-blue-500/30 h-96 w-80 absolute -bottom-40 -right-10 z-0 rounded-full" />
+					<div className="bg-blue-200/30 h-62 w-62 absolute bottom-20 right-20 z-0 rounded-full" />
+				</div>
+			</div>
+		);
 }
--- app/components/productList.tsx
+++ app/components/productList.tsx
@@ -1,39 +1,63 @@
 import { useEffect, useState } from "react";
 import type { ProductType } from "~/type";
 import ProductCard from "./productCard";
+import { CircularProgress } from "@mui/material";
 
-export default function ProductList({itemType}: {itemType: "pulsa" | "data"}) {
-    const [products, setProducts] = useState<ProductType[]>([]);
+export default function ProductList({
+	itemType,
+}: {
+	itemType: "pulsa" | "data";
+}) {
+	const [products, setProducts] = useState<ProductType[]>([]);
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            try {
-                const response = await fetch(`http://localhost:3000/items`);
-                if (response.ok) {
-                    const data = await response.json();
-                    setProducts(data);
-                } else {
-                    console.error("Failed to fetch products");
-                }
-            } catch (error) {
-                console.error("An error occurred while fetching products:", error);
-            }
-        };
+	useEffect(() => {
+		const fetchProducts = async () => {
+			try {
+				const response = await fetch(`http://localhost:3000/items`);
+				if (response.ok) {
+					const data = await response.json();
+					setProducts(data);
+				} else {
+					console.error("Failed to fetch products");
+				}
+			} catch (error) {
+				console.error("An error occurred while fetching products:", error);
+			}
+		};
 
-        fetchProducts();
-        return () => {
-            setProducts([]);
-        };
-    },[])
+		fetchProducts();
+		return () => {
+			setProducts([]);
+		};
+	}, []);
 
-    return (
-        <div>
-            <h2 className="text-2xl font-bold mb-4">{itemType === "pulsa" ? "Pulsa" : "Data"}</h2>
-            <div className="flex overflow-x-scroll gap-4 scrollbar">
-                {products.filter((product) => product.itemType === itemType).map((product) => (
-                    <ProductCard key={product.id} product={product} />
-                ))}
-            </div>
-        </div>
-    )
+	return (
+		<div className="relative z-10">
+			<h2 className="text-2xl font-bold mb-4 text-white">
+				{itemType === "pulsa" ? "Pulsa" : "Data"}
+			</h2>
+			{products.length === 0 ? (
+				<p className="text-gray-300 h-72 w-full flex items-center justify-center">
+					<CircularProgress size={100} color="inherit" />
+				</p>
+			) : (
+				<div className="flex overflow-x-scroll gap-4 scrollbar">
+					{products
+						.filter((product) => product.itemType === itemType)
+						.map((product, id) => {
+							if (id < 4)
+								return <ProductCard key={product.id} product={product} />;
+						})}
+					<a
+						href="/products"
+						className="group relative flex flex-col overflow-clip justify-center items-center bg-orange-400 text-white rounded-lg p-4 min-w-64 h-72">
+						<div className="absolute top-0 -left-10 bg-orange-100/20 w-46 h-28 rounded-full z-30" />
+						<div className="absolute top-20 -left-18 bg-orange-300/30 w-32 h-20 rounded-full z-30" />
+						<div className="absolute -bottom-4 -right-14 bg-orange-300/30 w-32 h-20 rounded-full z-30" />
+						<p className="text-2xl group-hover:italic font-semibold">More</p>
+					</a>
+				</div>
+			)}
+		</div>
+	);
 }
