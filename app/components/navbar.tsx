c653dbf12af271137ba55e7426605525469d9af5
alfazh123 <alfazh291@gmail.com>
2026-03-29 15:44:09 +0700

    fix navbar and index page

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,81 +1,105 @@
-import { MenuItem} from "@mui/material";
+import { History, Logout, Person } from "@mui/icons-material";
+import { MenuItem } from "@mui/material";
 import { useState } from "react";
 import type { UserType } from "~/type";
 
 export default function Navbar({ user }: { user?: UserType }) {
-    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-    const [open, setOpen] = useState(false);
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+	const [open, setOpen] = useState(false);
 
-    const menu = [
-        { label: "Profile", onClick: () => {setOpen(false)} },
-        { label: "Riwayat Transaksi", onClick: () => {setOpen(false)} },
-        { label: "Logout", onClick: () => {
-            localStorage.removeItem("user");
-            window.location.href = "/login";
-        } },
-    ]
+	const menu = [
+		{
+			label: "Profile",
+			icon: <Person />,
+			onClick: () => {
+				setOpen(false);
+			},
+		},
+		{
+			label: "Riwayat Transaksi",
+			icon: <History />,
+			onClick: () => {
+				setOpen(false);
+			},
+		},
+		{
+			label: "Logout",
+			icon: <Logout />,
+			onClick: () => {
+				localStorage.removeItem("user");
+				window.location.href = "/login";
+			},
+		},
+	];
 
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget);
-        setOpen(true);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-    const handleLogout = () => {
-        localStorage.removeItem("user");
-        window.location.href = "/login";
-    };
+	const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+		setAnchorEl(event.currentTarget);
+		setOpen((prev) => !prev);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+	const handleLogout = () => {
+		localStorage.removeItem("user");
+		window.location.href = "/login";
+	};
 
-    const initial = user?.name?.trim()?.charAt(0)?.toUpperCase() || "U";
+	const initial = user?.name?.trim()?.charAt(0)?.toUpperCase() || "U";
 
-    return  (
-        <div className="flex max-w-6xl mx-auto">
-            <nav className="sticky top-0 z-20 mt-4 w-full rounded-full border border-white/30 bg-linear-to-r from-slate-200 via-slate-100 to-slate-200 px-4 py-3 text-slate-900 shadow-xl backdrop-blur">
-                <div className="flex items-center justify-between gap-4">
-                    <a
-                        href="/"
-                        className="group inline-flex items-center gap-3"
-                    >
-                        <span className="grid h-10 w-10 place-items-center rounded-full bg-linear-to-br from-amber-400 to-orange-500 text-sm font-bold text-slate-900 shadow-lg transition-transform group-hover:scale-105">
-                            EC
-                        </span>
-                        <div className="leading-tight">
-                            <p className="text-base font-semibold tracking-wide">E-Commerce</p>
-                            <p className="text-xs text-slate-600">Smart shopping starts here</p>
-                        </div>
-                    </a>
+	return (
+		<div className="max-w-6xl mx-auto w-full">
+			<nav className="fixed top-0 z-20 mt-4 max-w-6xl w-full rounded-full border border-white/30 bg-linear-to-r from-slate-200 via-slate-100 to-slate-200 px-4 py-3 text-slate-900 shadow-sm backdrop-blur">
+				<div className="flex items-center justify-between gap-4">
+					<a href="/" className="group inline-flex items-center gap-3">
+						<span className="grid h-10 w-10 place-items-center rounded-full bg-linear-to-br from-amber-400 to-orange-500 text-sm font-bold text-slate-900 shadow-lg transition-transform group-hover:scale-105">
+							EC
+						</span>
+						<div className="leading-tight">
+							<p className="text-base font-semibold tracking-wide">
+								E-Commerce
+							</p>
+							<p className="text-xs text-slate-600">
+								Smart shopping starts here
+							</p>
+						</div>
+					</a>
 
-                    {user?.name ? (
-                        <div className="flex items-center gap-2">
+					{user?.name ? (
+						<div className="flex items-center gap-2">
+							<button
+								type="button"
+								id="basic-button"
+								onClick={handleClick}
+								className="flex  items-center gap-2 rounded-full border border-gray-300 px-3 py-1 text-sm font-medium text-slate-700 transition ring-2  ring-orang-200 hover:ring-orange-400 focus:ring-orange-400">
+								<span className="grid h-7 w-7 place-items-center rounded-full bg-orange-300 font-semibold text-slate-900">
+									{initial}
+								</span>
+								<span className="max-w-32 truncate">Hi, {user.name}</span>
+							</button>
 
-                            <button
-                                type="button"
-                                id="basic-button"
-                                onClick={handleClick}
-                                className="flex  items-center gap-2 rounded-full border border-gray-300 px-3 py-1 text-sm font-medium text-slate-700 transition ring-2  ring-orang-200 hover:ring-orange-400 focus:ring-orange-400"
-                            >
-                                <span className="grid h-7 w-7 place-items-center rounded-full bg-orange-300 font-semibold text-slate-900">
-                                    {initial}
-                                </span>
-                                <span className="max-w-32 truncate">Hi, {user.name}</span>
-                            </button>
-
-                            {open && (
-                                <div className="fixed z-10 right-0 top-14 " onClick={handleClose}>
-                                    <div className="bg-slate-100 rounded-2xl" onClick={handleClose}>
-                                        {menu.map((item) => (
-                                            <MenuItem key={item.label} onClick={() => {
-                                                item.onClick();
-                                                handleClose();
-                                            }}>
-                                                {item.label}
-                                            </MenuItem>
-                                        ))}
-                                    </div>
-                                </div>
-                            )}
-                            {/* <Menu
+							{open && (
+								<div
+									className="fixed z-10 right-0 top-14 "
+									onClick={handleClose}>
+									<div
+										className="bg-slate-100 rounded-2xl py-1"
+										onClick={handleClose}>
+										{menu.map((item) => (
+											<div
+												key={item.label}
+												onClick={() => {
+													item.onClick();
+													handleClose();
+												}}
+												className="flex gap-2 h-10 m-4 px-1 items-center hover:bg-slate-200">
+												{item.icon}
+												<p>{item.label}</p>
+											</div>
+										))}
+									</div>
+								</div>
+							)}
+							{/* <Menu
                                 id="basic-menu"
                                 anchorEl={anchorEl}
                                 open={open}
@@ -97,25 +121,23 @@ export default function Navbar({ user }: { user?: UserType }) {
                                 <MenuItem onClick={handleClose}>Profile</MenuItem>
                                 <MenuItem onClick={handleLogout}>Logout</MenuItem>
                             </Menu> */}
-                        </div>
-                    ) : (
-                        <div className="flex items-center">
-                            <a
-                                href="/register"
-                                className="rounded-l-full border border-black/20 pl-4 pr-2 py-2 text-sm font-medium text-slate-600 transition hover:bg-white/10"
-                            >
-                                Register
-                            </a>
-                            <a
-                                href="/login"
-                                className="rounded-r-full bg-orange-400 border border-orange-400/20 px-4 py-2 text-sm font-semibold text-slate-900 transition hover:bg-orange-500"
-                            >
-                                Login
-                            </a>
-                        </div>
-                    )}
-                </div>
-            </nav>
-        </div>
-    )
+						</div>
+					) : (
+						<div className="flex items-center">
+							<a
+								href="/register"
+								className="rounded-l-full border border-black/20 pl-4 pr-2 py-2 text-sm font-medium text-slate-600 transition hover:bg-white/10">
+								Register
+							</a>
+							<a
+								href="/login"
+								className="rounded-r-full bg-orange-400 border border-orange-400/20 px-4 py-2 text-sm font-semibold text-slate-900 transition hover:bg-orange-500">
+								Login
+							</a>
+						</div>
+					)}
+				</div>
+			</nav>
+		</div>
+	);
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,32 +1,41 @@
-import { useEffect, useState } from "react";
 import type { Route } from "./+types/home";
 import PromoList from "~/components/promoList";
 import ProductGroup from "~/components/productGroup";
+import HookSection from "~/components/hookSection";
+import HeroSection from "~/components/hero";
 
 export function meta({}: Route.MetaArgs) {
 	return [
-		{ title: "New React Router App" },
-		{ name: "description", content: "Welcome to React Router!" },
+		{ title: "E-com" },
+		{
+			name: "E-com fast and reliable",
+			content: "get your exclusive deals and benefits",
+		},
 	];
 }
 
 export default function Home() {
 	return (
-		<div className="flex flex-col max-w-6xl mx-auto pt-10">
+		<div className="flex flex-col pt-10">
+			<HeroSection />
+			<div
+				id="hook"
+				className="flex-1 flex flex-col items-center justify-center gap-4 text-center md:my-32 my-10">
+				<HookSection
+					title="Start now and get your benefits!"
+					description="Join our community today and enjoy exclusive deals, special offers, and premium benefits that are available only to our valued members who trust us!"
+				/>
+				<HookSection
+					title="Watch films without buffering!"
+					description="Experience seamless streaming with our high-speed internet packages, designed to keep you connected and entertained without interruptions. Say goodbye to buffering and hello to endless entertainment!"
+				/>
+				<HookSection
+					title="Enjoy your gaming without lag!"
+					description="Level up your gaming experience with our high-speed internet packages, designed to keep you connected and competitive without interruptions. Say goodbye to lag and hello to seamless gaming!"
+				/>
+			</div>
 			<PromoList />
 			<ProductGroup />
-			<div className="flex-1 flex flex-col items-center justify-center gap-4 text-center">
-				{/* <h1 className="text-4xl font-bold tracking-tight text-slate-900">
-					Welcome to E-Commerce
-				</h1>
-				<p className="text-lg text-slate-700">
-					Discover a world of products at your fingertips. Shop now and
-					experience the best online shopping experience!
-				</p> */}
-			</div>
-			<footer className="py-4 text-center text-sm text-slate-500">
-				&copy; {new Date().getFullYear()} E-Commerce. All rights reserved.
-			</footer>
 		</div>
 	);
 }
